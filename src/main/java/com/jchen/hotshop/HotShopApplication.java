40ae186dbba49fc3f8328d9b1ca9e9c444b7eb9a
darknessD <964951139@qq.com>
2019-03-27 21:17:15 +0800

    upload the project

---
+++ src/main/java/com/jchen/hotshop/HotShopApplication.java
@@ -0,0 +1,13 @@
+package com.jchen.hotshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotShopApplication.class, args);
+    }
+
+}
